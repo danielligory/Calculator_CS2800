a9dca10c786b9392ae4e5ed5043b68b0b20dddd7
danielligory <daniel.ligory@gmail.com>
2022-11-11 12:17:20 +0000

    Have working Entry class with proper TDD in TestEntry class

--- Calculator/src/main/java/calculator/Entry.java
+++ Calculator/src/main/java/calculator/Entry.java
@@ -6,8 +6,62 @@ package calculator;
  * @author Daniel Ligory
  * 
  */
-
 public class Entry {
+  private float number;
+  private Symbol other;
+  private Type type;
+  private String str;
 
+  /**
+   * A constructor assigning the value to number.
+   *
+   * @param value number input by user
+   */
+  public Entry(float value) {
+    number = value;
+  }
+
+
+  /**
+   * @return the number
+   * @throws bad type Exception
+   */
+  public float getValue(){
+    return this.number;
+  }
+
+  /**
+   * A constructor assigning the value to other.
+   *
+   * @param which symbol input by user
+   */
+  public Entry(Symbol which) {
+    other = which;
+  }
   
+  /**
+   * @return the other
+   * @throws bad type Exception
+   */
+  public Symbol getSymbol(){
+    return this.other;
+  }
+
+
+  /**
+   * @return the str
+   * @throws bad type Exception
+   */
+  public String getString(){
+    return str;
+  }
+
+  /**
+   * @return the type
+   */
+  public Type getType() {
+    return type;
+  }
+
+
 }
--- Calculator/src/test/java/calculator/TestEntry.java
+++ Calculator/src/test/java/calculator/TestEntry.java
@@ -2,12 +2,28 @@ package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+//import calculator.Entry;
 
 class TestEntry {
 
   @Test
-  void test() {
-    fail("Not yet implemented");
-  }
+  void testContructor1(){
+    // Test 1 Check if constructor constructed properly.
+    Entry e = new Entry(100f);
+    assertEquals(100f, e.getValue());
+    // Test 2 Check if constructor constructed a longer float properly.
+    Entry e1 = new Entry(1230090213901239f);
+    assertEquals(1230090213901239f, e1.getValue());
 
+  }
+  
+  @Test
+  void testContructor2(){
+    // Test 1 Check if constructor constructed properly.
+    Entry e = new Entry(Symbol.LEFT_BRACKET);
+    assertEquals(Symbol.LEFT_BRACKET, e.getSymbol());
+    
+  }
+  
 }
+
