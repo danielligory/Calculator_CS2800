feade1b471bd55045f93695d7aad35d53eaa84db
danielligory <daniel.ligory@gmail.com>
2022-11-17 16:17:23 +0000

    Add push() method

---
+++ Calculator/src/main/java/calculator/NumStack.java
@@ -0,0 +1,33 @@
+package calculator;
+
+/**
+ * Since reverse Polish Evaluation only uses a Stack of numeric values we provide a facade to hide details.
+ * @author Daniel Ligory
+ *
+ */
+
+public class NumStack {
+  private int size;
+  private Stack numStack = new Stack();
+
+  /**
+   * Adds a new Entry to Stack.
+   * @param i argument that is added to Stack
+   */
+  public void push(float i) {
+    numStack.push(new Entry (i));
+    size ++;
+  }
+
+  
+  
+  
+  /**
+   * Used to tell us how many entries are in the Stack.
+   * @return the size
+   */
+  public int size() {
+    return size;
+  }
+}
+
--- Calculator/src/test/java/calculator/TestNumStack.java
+++ Calculator/src/test/java/calculator/TestNumStack.java
@@ -1,13 +1,38 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestNumStack {
+  private NumStack testNumStack;
 
-  @Test
-  void test() {
-    fail("Not yet implemented");
+  @BeforeEach
+  public void createNumStack() {
+   // Test 1 Check if stack is created.
+    testNumStack = new NumStack();
   }
+  
+  @Test
+  public void testPush() {
+    // Test 1 Check if Stack is created properly.
+    assertEquals(0, testNumStack.size(), "Check if Stack is working, size of Stack should be 0");
+    // Test 2 Check if one entry was pushed properly.
+    testNumStack.push(1234.123f);
+    assertEquals(1, testNumStack.size(), "Check if size of Stack is size 1");
+    // Test 3 Check if two entries was pushed properly.
+    testNumStack.push(5214124.123f);
+    assertEquals(2, testNumStack.size(), "Check if size of Stack is size 2");
+    // Test 4 Check if 36 entries was pushed properly.
+    int i = 0;
+    while (i != 34) {
+      testNumStack.push(4214f);
+      i ++;
+    }
+    assertEquals(36, testNumStack.size(), "Check is size of Stack is 36");
+
+  }
+  
+  
 
 }
